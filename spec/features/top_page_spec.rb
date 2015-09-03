ca536c679e64de627b1a14cbabfee78293888714
craftpaperbag <kazu11518@gmail.com>
2015-09-03 23:08:00 +0900

    アカウント登録テスト

--- spec/features/top_page_spec.rb
+++ spec/features/top_page_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
 describe 'Top page' do
+
   before { visit '/' }
   it 'アプリ名を表示する' do
     expect(page).to have_css 'h1', text: 'Xzap'
+  end
+
+  shared_context :send_account_form_with do |email, password|
+    before do
+      visit '/'
+      fill_in 'email', with: email
+      fill_in 'password', with: password
+      find('input[type=submit]').click
+    end
+  end
+
+  context do
+    include_context :send_account_form_with, 'email@email.com', 'password'
+    it 'ユーザー登録する' do
+      expect(User.first(email: 'email@email.com')).not_to eq nil
+    end
   end
 end
